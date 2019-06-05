d59669bfeca1350e75a6d97bf489c2eabe2141c4
roberta-randazzo-consultant <roberta.randazzo.consultant@myaxa.com>
2019-06-05 14:06:57 +0200

    set custom user account

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -8,6 +8,7 @@ public class Email {
     private String passowrd;
     private String department;
     private int defaultPasswordLenght=8;
+    private String companySuffix="aeycompany.com";
     private int mailboxCapacity;
     private String alternateEmail;
 
@@ -20,17 +21,19 @@ public class Email {
         this.department = setDepartment();
         System.out.println("Department is : " + this.department);
         this.passowrd= randomPassword(defaultPasswordLenght);
-        System.out.println(" YOUr Password is : "+this.passowrd);
+        System.out.println(" Your Password is : "+this.passowrd);
+        String email = this.firstName.toLowerCase() + "." + this.lastName.toLowerCase()+"@"+ this.department + "."+ this.companySuffix;
+        System.out.println(" Your Email is : "+email);
     }
 
     private String setDepartment() {
         System.out.println("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting \n0 for none\n Enter Department Code : ");
         Scanner in= new Scanner(System.in);
         int deepChoice = in.nextInt();
-        if (deepChoice == 1){return "Sales";}
-    	else if(deepChoice == 2) {return "Development";}
-    	else if(deepChoice == 3) {return "Accounting";}
-    	else if(deepChoice == 0) {return "None";}
+        if (deepChoice == 1){return "sales";}
+    	else if(deepChoice == 2) {return "development";}
+    	else if(deepChoice == 3) {return "accounting";}
+    	else if(deepChoice == 0) {return "none";}
         else {return null;}
     }
 
--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -1,9 +1,20 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class EmailApp {
 
     public static void  main(String[] args){
-        Email eml = new Email("Jhon", "Smith");
+        //Email eml = new Email("Jhon", "Smith");
+
+        System.out.println("Enter Name : ");
+        Scanner name= new Scanner(System.in);
+        String nameChoice = name.next();
+        System.out.println("Enter Lastname : ");
+        Scanner lastname= new Scanner(System.in);
+        String lastnameChoice = lastname.next();
+
+        Email eml = new Email(nameChoice, lastnameChoice);
 
     }
 }
